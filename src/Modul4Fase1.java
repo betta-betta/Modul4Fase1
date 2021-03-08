2d81154c625212eeb505a7fc5584a566e6d99097
betta <betta@DESKTOP-LJQJ287>
2021-03-08 20:39:05 +0100

    Modul4Nivell1

---
+++ src/Modul4Fase1.java
@@ -0,0 +1,20 @@
+
+public class Modul4Fase1 {
+
+	public static void main(String[] args) {
+		int euro5=5;
+		int euro10=10;
+		int euro20=20;
+		int euro50=50;
+		int euro100=100;
+		int euro200=200;
+		int euro500=500;
+		double total=0;
+		
+		String[] menu = new String[5];
+		Integer[] preu = new Integer[5];
+		
+
+	}
+
+}
